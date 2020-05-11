b5df2f07ec8094293cf2c8a858cf04277e9cad39
chicagoist <le.legioner@gmail.com>
2020-05-11 13:45:14 +0300

    11 May 2020

---
+++ HomeTasks/07_29_task.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+В переменной $date лежит дата в формате '2013-12-31'. 
+Преобразуйте эту дату в формат '31.12.2013'. 
+ */
+
+function myChangeDate( string $str ) : string {
+    
+    $arr = explode('-', $str);
+    return $arr[2] . '.' . $arr[1] . '.' . $arr[0];
+    
+}
+
+$date = '2013-12-31';
+
+echo myChangeDate($date);
+
+
+
