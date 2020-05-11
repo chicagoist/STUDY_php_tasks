acd5b23e39b15925ae6b88a8d89ba6e464934967
chicagoist <le.legioner@gmail.com>
2020-05-11 17:38:46 +0300

    10 May 2020

--- 07_20_task.php
+++ 07_20_task.php
@@ -1,8 +1,25 @@
 <?php
 
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+<<<<<<< HEAD
+ Дана строка $str. Вырежите из нее подстроку с 3-го символа (отсчет с нуля), 
+ 5 штук и вместо нее вставьте '!!!'. 
  */
 
+function mySubstr_Raplace( string $str ) {
+    
+    echo substr_replace($str, '!!!', 3, 5 ); // не работает с кириллицей
+    
+    
+   
+    
+}
+
+$str = "Given the string $ str. Cut from it a substring from the 3rd character
+        (counting from scratch), 5 pieces and insert '!!!' instead.";
+
+echo 'Эквивалент :<br />';
+print_r($str);
+echo '<br /><br />';
+
+mySubstr_Raplace($str);
